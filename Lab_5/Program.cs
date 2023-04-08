67fca77f48ec4f31e70699272f76404eeb32bd25
Twinker25 <shashokd@gmail.com>
2023-04-08 12:04:08 +0300

    Add Task3

--- Lab_5/Program.cs
+++ Lab_5/Program.cs
@@ -14,103 +14,146 @@ namespace Lab_5
                 switch (choice)
                 { 
                 case 1:
-                    Employer employer1 = new Employer();
-                    Employer employer2 = new Employer();
-                    employer1.Input();
-                    Console.WriteLine();
-                    employer2.Input();
-                    Console.WriteLine("\nShow info:");
-                    employer1.Show();
-                    Console.WriteLine();
-                    employer2.Show();
-                    Console.WriteLine();
-                    employer1 += 100;
-                    employer2 += 100;
-                    Console.Write("\nSalary of employer 1: ");
-                    employer1.ShowSalary();
-                    Console.Write("\nSalary of employer 2: ");
-                    employer2.ShowSalary();
-                    employer1 -= 50;
-                    employer2 -= 25;
-                    Console.Write("\nSalary of employer 1: ");
-                    employer1.ShowSalary();
-                    Console.Write("\nSalary of employer 2: ");
-                    employer2.ShowSalary();
-                    Console.WriteLine();
+                        Employer employer1 = new Employer();
+                        Employer employer2 = new Employer();
+                        employer1.Input();
+                        Console.WriteLine();
+                        employer2.Input();
+                        Console.WriteLine("\nShow info:");
+                        employer1.Show();
+                        Console.WriteLine();
+                        employer2.Show();
+                        Console.WriteLine();
+                        employer1 += 100;
+                        employer2 += 100;
+                        Console.Write("\nSalary of employer 1: ");
+                        employer1.ShowSalary();
+                        Console.Write("\nSalary of employer 2: ");
+                        employer2.ShowSalary();
+                        employer1 -= 50;
+                        employer2 -= 25;
+                        Console.Write("\nSalary of employer 1: ");
+                        employer1.ShowSalary();
+                        Console.Write("\nSalary of employer 2: ");
+                        employer2.ShowSalary();
+                        Console.WriteLine();
 
-                    if (employer1 == employer2)
-                    {
-                        Console.WriteLine("Employees have equal salary.");
-                    }
+                        if (employer1 == employer2)
+                        {
+                            Console.WriteLine("\nEmployees have equal salary.");
+                        }
 
-                    if (employer1 > employer2)
-                    {
-                        Console.WriteLine("Employee 1 has higher salary than Employee 2.");
-                    }
+                        if (employer1 > employer2)
+                        {
+                            Console.WriteLine("\nEmployee 1 has higher salary than Employee 2.");
+                        }
 
-                    else if (employer1 < employer2)
-                    {
-                        Console.WriteLine("Employee 2 has higher salary than Employee 1.");
-                    }
+                        else if (employer1 < employer2)
+                        {
+                            Console.WriteLine("\nEmployee 2 has higher salary than Employee 1.");
+                        }
 
-                    if (employer1 != employer2)
-                    {
-                        Console.WriteLine("Employees have different salary.");
-                    }
+                        if (employer1 != employer2)
+                        {
+                            Console.WriteLine("\nEmployees have different salary.");
+                        }
                     
-                    if (employer1.Equals(employer2))
-                    {
-                        Console.WriteLine("Employees are the same person.");
-                    }
-                    break;
+                        if (employer1.Equals(employer2))
+                        {
+                            Console.WriteLine("\nEmployees are the same person.");
+                        }
+                        break;
                 case 2:
-                    Matrix matrix1 = new Matrix(2, 2);
-                    Matrix matrix2 = new Matrix(2, 2);
-                    matrix1.GenMatrix();
-                    matrix2.GenMatrix();
-                    Console.WriteLine("Matrix 1: ");
-                    matrix1.Show();
-                    Console.WriteLine("\nMatrix 2: ");
-                    matrix2.Show();
-                    Console.WriteLine("\nSum of matrix: ");
-                    Matrix sum = matrix1 + matrix2;
-                    sum.Show();
-                    Console.WriteLine("\nSub of matrix: ");
-                    Matrix sub = matrix1 - matrix2;
-                    sub.Show();
-                    Console.WriteLine("\nMult of matrix: ");
-                    Matrix mult = matrix1 * matrix2;
-                    mult.Show();
-                    Console.WriteLine("\nMult matrix 1 on number: ");
-                    Console.Write("Enter number: ");
-                    int num = int.Parse(Console.ReadLine());
-                    Matrix mult1 = matrix1 * num;
-                    mult1.Show();
+                        Matrix matrix1 = new Matrix(2, 2);
+                        Matrix matrix2 = new Matrix(2, 2);
+                        matrix1.GenMatrix();
+                        matrix2.GenMatrix();
+                        Console.WriteLine("Matrix 1: ");
+                        matrix1.Show();
+                        Console.WriteLine("\nMatrix 2: ");
+                        matrix2.Show();
+                        Console.WriteLine("\nSum of matrix: ");
+                        Matrix sum = matrix1 + matrix2;
+                        sum.Show();
+                        Console.WriteLine("\nSub of matrix: ");
+                        Matrix sub = matrix1 - matrix2;
+                        sub.Show();
+                        Console.WriteLine("\nMult of matrix: ");
+                        Matrix mult = matrix1 * matrix2;
+                        mult.Show();
+                        Console.WriteLine("\nMult matrix 1 on number: ");
+                        Console.Write("Enter number: ");
+                        int num = int.Parse(Console.ReadLine());
+                        Matrix mult1 = matrix1 * num;
+                        mult1.Show();
 
-                    if(matrix1 == matrix2)
-                    {
-                        Console.WriteLine("Matrix are similar!");
-                    }
+                        if(matrix1 == matrix2)
+                        {
+                            Console.WriteLine("\nMatrix are similar!");
+                        }
 
-                    if (matrix1 != matrix2)
-                    {
-                        Console.WriteLine("Matrix aren`t similar!");
-                    }
+                        if (matrix1 != matrix2)
+                        {
+                            Console.WriteLine("\nMatrix aren`t similar!");
+                        }
 
-                    if (matrix1.Equals(matrix2))
-                    {
-                        Console.WriteLine("Matrix are equal similar!");
-                    }
-                    break;
+                        if (matrix1.Equals(matrix2))
+                        {
+                            Console.WriteLine("\nMatrix are equal similar!");
+                        }
+                        break;
                 case 3:
+                        City city1 = new City();
+                        City city2 = new City();
+                        city1.Input();
+                        Console.WriteLine();
+                        city2.Input();
+                        Console.WriteLine("\nShow info:");
+                        city1.Show();
+                        Console.WriteLine();
+                        city2.Show();
+                        Console.WriteLine();
+                        Console.Write("\nPopulation of city 1: ");
+                        city1.ShowHumanity();
+                        Console.Write("\nPopulation of city 2: ");
+                        city2.ShowHumanity();
+                        Console.Write("\nPopulation of city 1: ");
+                        city1.ShowHumanity();
+                        Console.Write("\nPopulation of city 2: ");
+                        city2.ShowHumanity();
+                        Console.WriteLine();
 
-                    break;
+                        if (city1 == city2)
+                        {
+                            Console.WriteLine("\nCity have equal population.");
+                        }
+
+                        if (city1 > city2)
+                        {
+                            Console.WriteLine("\nCity 1 has higher population than City 2.");
+                        }
+
+                        else if (city1 < city2)
+                        {
+                            Console.WriteLine("\nCity 2 has higher population than City 1.");
+                        }
+
+                        if (city1 != city2)
+                        {
+                            Console.WriteLine("\nCities have different population.");
+                        }
+
+                        if (city1.Equals(city2))
+                        {
+                            Console.WriteLine("\nCities are the equals.");
+                        }
+                        break;
                 case 4:
 
-                    break;
+                        break;
                 default:
-                    Console.WriteLine("Error! Try again!");
-                    break;
+                        Console.WriteLine("Error! Try again!");
+                        break;
                 }
             } while (choice < 1 || choice > 4);
         }
---
+++ Lab_5/Task3.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class City
+    {
+        private string name;
+        private string country;
+        private int humanity;
+        private int phonecode;
+        private string[] district;
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetCountry(string country)
+        {
+            this.country = country;
+        }
+        public string GetCountry()
+        {
+            return country;
+        }
+        public void SetHumanity(int humanity)
+        {
+            this.humanity = humanity;
+        }
+        public int GetHumanity()
+        {
+            return humanity;
+        }
+        public void SetPhonecode(int phonecode)
+        {
+            this.phonecode = phonecode;
+        }
+        public int GetPhonecode()
+        {
+            return phonecode;
+        }
+        public void SetDistrict(string[] district)
+        {
+            this.district = district;
+        }
+        public string[] GetDistrict()
+        {
+            return district;
+        }
+
+        public static City operator +(City city, int amount)
+        {
+            city.humanity += amount;
+            return city;
+        }
+
+        public static City operator -(City city, int amount)
+        {
+            city.humanity -= amount;
+            return city;
+        }
+
+        public static bool operator ==(City city1, City city2)
+        {
+            if (ReferenceEquals(city1, city2))
+            {
+                return true;
+            }
+
+            if ((object)city1 == null || (object)city2 == null)
+            {
+                return false;
+            }
+
+            return city1.humanity == city2.humanity;
+        }
+
+        public static bool operator !=(City city1, City city2)
+        {
+            return !(city1 == city2);
+        }
+
+        public static bool operator <(City city1, City city2)
+        {
+            return city1.humanity < city2.humanity;
+        }
+
+        public static bool operator >(City city1, City city2)
+        {
+            return city1.humanity > city2.humanity;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            City city = obj as City;
+
+            if ((object)city == null)
+            {
+                return false;
+            }
+
+            return humanity == city.humanity;
+        }
+
+        public void ShowHumanity()
+        {
+            Console.WriteLine($"\nPopulation: {humanity}");
+        }
+
+        public void Input()
+        {
+            Console.Write("Enter city name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter country name: ");
+            country = Console.ReadLine();
+
+            Console.Write("Enter city population: ");
+            humanity = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter phone code: ");
+            phonecode = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number of districts: ");
+            int numDistrict = int.Parse(Console.ReadLine());
+
+            district = new string[numDistrict];
+
+            for (int i = 0; i < numDistrict; i++)
+            {
+                Console.Write($"Enter name of district {i + 1}: ");
+                district[i] = Console.ReadLine();
+            }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"\nResult:\nCity: {name}");
+            Console.WriteLine($"Country: {country}");
+            Console.WriteLine($"Population: {humanity}");
+            Console.WriteLine($"Phone code: {phonecode}");
+            Console.WriteLine("Districts: ");
+            foreach (string district in district)
+            {
+                Console.WriteLine($"- {district}");
+            }
+        }
+    }
+}
